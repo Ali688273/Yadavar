79fed3cd049b8ad948ab88392ceda9911099409d
Ali688273 <iiii688273@gmail.com>
2026-09-26 11:06:16 +0330

    Restore geofences after device reboot

---
+++ app/src/main/java/com/yadavar/app/core/GeofenceBootReceiver.kt
@@ -0,0 +1,11 @@
+package com.yadavar.app.core
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class GeofenceBootReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if(intent.action==Intent.ACTION_BOOT_COMPLETED) GeofenceManager.registerAll(context)
+    }
+}
